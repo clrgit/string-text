b22ac4f4e1bc9d2b3ddc45c615f080aa71636286
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2024-06-05 20:23:29 +0200

    * Renamed #outdent -> #align

--- lib/string-text.rb
+++ lib/string-text.rb
@@ -6,7 +6,20 @@ module String::Text
   class Error < StandardError; end
   
   refine String do
-    def undent
+    # Indent or outdent a block of text to the given column. It uses the indent
+    # of the non-empty line as the indent of the whole block that is then
+    # aligned as a whole (including internal indents) to the given column
+    #
+    # It is often handy when you're calling methods with a %(...) argument and
+    # don't want weird indentation in your output
+    #
+    #   puts %(
+    #     This line will start at column 1
+    #       This line will start at column 3
+    #   ).align
+    #
+    def align(column = 1)
+      column == 1 or raise NotImplementedError
       lines = self.split(/\n/)
       lines.shift while !lines.empty? && !(lines.first =~ /^(\s*)\S/)
       return "" if lines.empty?
@@ -21,8 +34,9 @@ module String::Text
       r.join("\n").chomp
     end
 
-    # Converts a string to a boolean so that "true" becomes true and "false" and
-    # the empty string becomes false. Should actually go in to a 
+    # Converts a string to a boolean so that "true" becomes true and that
+    # "false" and the empty string becomes false. Any other string is an error
+    #
     def to_b
       case self
         when "true"; true
--- spec/string-text_spec.rb
+++ spec/string-text_spec.rb
@@ -5,34 +5,34 @@ describe "String::Text" do
     expect(String::Text::VERSION).not_to be_nil
   end
 
-  describe "#undent" do
+  describe "#align" do
     context "when given a string w/o newlines" do
       it "removes initial indent" do
-        expect("  text".undent).to eq "text"
+        expect("  text".align).to eq "text"
       end
       it "removes final whitespace" do
-        expect("text  ".undent).to eq "text"
+        expect("text  ".align).to eq "text"
       end
     end
     context "when given a multiline string" do
       it "ignores initial empty lines" do
-        expect("\n\ntext".undent).to eq "text"
+        expect("\n\ntext".align).to eq "text"
       end
       it "removes the initial indent from the first line" do
-        expect("\n  text".undent).to eq "text"
+        expect("\n  text".align).to eq "text"
       end
       it "removes the initial indent from the remaining lines" do
-        expect("  line1\n    line2\n".undent).to eq "line1\n  line2"
+        expect("  line1\n    line2\n".align).to eq "line1\n  line2"
       end
       it "removes final blanks from all lines" do
-        expect("  line1\n    \n    line2\n".undent).to eq "line1\n\n  line2"
+        expect("  line1\n    \n    line2\n".align).to eq "line1\n\n  line2"
       end
       it "internal empty lines are not indented" do
-        expect("  line1\n    \n    line2\n".undent).to eq "line1\n\n  line2"
+        expect("  line1\n    \n    line2\n".align).to eq "line1\n\n  line2"
       end
       it "removes final whitespace" do
-        expect("text  \n".undent).to eq "text"
-        expect("line1  \nline2  \n".undent).to eq "line1\nline2"
+        expect("text  \n".align).to eq "text"
+        expect("line1  \nline2  \n".align).to eq "line1\nline2"
       end
     end
   end
