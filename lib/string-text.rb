7c2b294baf6051b35ac543ae482888270d596972
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2024-06-05 21:39:16 +0200

    Tuned #align

--- lib/string-text.rb
+++ lib/string-text.rb
@@ -8,10 +8,11 @@ module String::Text
   refine String do
     # Indent or outdent a block of text to the given column. It uses the indent
     # of the non-empty line as the indent of the whole block that is then
-    # aligned as a whole (including internal indents) to the given column
+    # aligned as a whole (including internal indents) to the given column.
+    # Initial and final empty lines are ignored
     #
-    # It is often handy when you're calling methods with a %(...) argument and
-    # don't want weird indentation in your output
+    # #align is often handy when you call methods with a %(...) argument
+    # and don't want weird indentation in your output
     #
     #   puts %(
     #     This line will start at column 1
@@ -21,17 +22,11 @@ module String::Text
     def align(column = 1)
       column == 1 or raise NotImplementedError
       lines = self.split(/\n/)
+      lines.pop while !lines.empty? && !(lines.last =~ /^\s*\S/)
       lines.shift while !lines.empty? && !(lines.first =~ /^(\s*)\S/)
       return "" if lines.empty?
       indent = $1.size
-      r = []
-      while line = lines.shift&.rstrip
-        r << (line[indent..-1] || "")
-      end
-      while !r.empty? && r.last =~ /^\s*$/
-        r.pop
-      end
-      r.join("\n").chomp
+      lines.map { |line| line[indent..-1].rstrip || "" }.join("\n")
     end
 
     # Converts a string to a boolean so that "true" becomes true and that
